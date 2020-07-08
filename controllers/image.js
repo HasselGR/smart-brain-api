c1ac516c95e54193cf46789b71846b2b8c7864da
David Gamboa <gamboadavid1998@gmail.com>
2020-07-08 19:46:18 -0400

    adding files!

---
+++ controllers/image.js
@@ -0,0 +1,37 @@
+const Clarifai = require('clarifai');
+const { response } = require('express');
+
+
+const app = new Clarifai.App({
+    apiKey: '12efde3cf0e54559a8b37362ca344933'
+  });
+
+
+
+  const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err =>res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage =(req, res, db) =>{
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
